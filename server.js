addc77d835dba1b8233ac3457c183f87463bd4f0
Sergey Demyanov <s.demyanov@gmail.com>
2016-03-24 16:23:07 -0400

    catch unhandled exceptions

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ var bodyParser = require('body-parser');
 // increase max sockets
 require('http').globalAgent.maxSockets = 1000;
 
+
+// catch the uncaught errors that weren't wrapped in a domain or try catch statement
+// do not use this in modules, but only in applications, as otherwise we could have multiple of these bound
+process.on('uncaughtException', function(err) {
+    // handle the error safely
+    logger.error(err);
+});
+
 var app = express();
 
 // use winston stream for express logger middleware
