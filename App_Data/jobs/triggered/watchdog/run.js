df2a8258c07908bf8eb314e94a3144ac143f1356
Sergey Demyanov <s.demyanov@gmail.com>
2016-03-25 14:24:13 -0400

    added missing library ref

--- App_Data/jobs/triggered/watchdog/run.js
+++ App_Data/jobs/triggered/watchdog/run.js
@@ -6,6 +6,7 @@ require('http').globalAgent.maxSockets = 100;
 
 var needle = require('./../../../../node_modules/needle-retry');
 var winston = require('./../../../../node_modules/winston');
+var _ = require('./../../../../node_modules/lodsash');
 var log = new (winston.Logger)({
     transports: [
         new (winston.transports.Console)({'timestamp':true})
